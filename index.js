5d716fb2eb06d7120e4ae6a99b0feab8f1db17a5
zhengxiaoyao0716 <1499383852@qq.com>
2018-01-18 20:52:12 +0800

    v0.2.13

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const head = `<style>
     .Markplus-catalog {
         position: relative;
-        margin-right: 10px;
         transition: margin-right 0.5s;
-    }
-    .Markplus-catalog.fold {
-        margin-right: 50px;
     }
 
     .Markplus-catalog>a.fold {
@@ -44,20 +40,61 @@ const head = `<style>
         margin: 10px 30px 10px 12px;
         padding-left: 8px;
     }
+    .Markplus-catalog>.Tabel span {
+        word-break: break-all;
+    }
+    .Markplus-catalog>.Tabel span.tab-1 {
+        word-break: break-word;
+    }
     .Markplus-catalog>.Tabel span.tab:not(:hover) {
         color: #9ff;
     }
-    .Markplus-catalog>.Tabel span.tab:hover {
+    .Markplus-catalog>.Tabel span.tab:hover:not(.tab-1) {
         border-left: 3px solid #fff;
         padding-left: 5px;
     }
-    .Markplus-catalog>.Tabel span.tab-1 { text-decoration: underline; font-weight: bold; margin-top: 30px; margin-bottom: 20px; }
-    ${new Array(4).fill().map((_, index) => `.Markplus-catalog>.Tabel span.tab-${2 + index} { margin-left: ${12 + 10 * index}px; }`).join('\n    ')}
+    .Markplus-catalog>.Tabel span.tab-1 {
+        text-decoration: underline;
+        font-weight: bold;
+        margin-top: 30px;
+        margin-bottom: 20px;
+        text-align: center;
+        border: 1px solid;
+        padding: 3px;
+    }
+    .Markplus-catalog>.Tabel span.tab-2 { border-top: 1px solid; border-bottom: 1px dashed; margin-top: 20px; }
+    ${new Array(4).fill().map((_, index) => `.Markplus-catalog>.Tabel span.tab-${2 + index} { margin-left: ${12 + 15 * index}px; }`).join('\n    ')}
+    
+    .Markplus-catalog>span.top {
+        position: fixed;
+        bottom: 0;
+        width: 30vw;
+        transition: top 0.5s, width 0.5s;
+        display: block;
+        text-align: center;
+        cursor: pointer;
+        background: linear-gradient(0, #6cf, rgba(0, 0, 0, 0));
+        color: #9ff;
+    }
+    .Markplus-catalog.fold>span.top {
+        width: 0vw;
+    }
+    .Markplus-catalog>span.top:hover {
+        color: #fff;
+    }
+    .Markplus-catalog.fold>span.top:hover {
+        color: #6cf;
+    }
 </style><style id="markplusCatalogDynamicStyle"></style>`;
 
 const RenderCatalog = (self, fold) => ({
     head: () => head,
     code: () => `(container => {
+        window.addEventListener('load', () => ((parent) => {
+            parent.style.display = 'flex';
+            parent.insertBefore(container, Markplus.container);
+        })(Markplus.container.parentElement));
+
         container.classList.add('Markplus-catalog');
         container.appendChild((span => (span.innerHTML = '<!-- markplus-plugin-render-catalog -->', span))(document.createElement('span')));
 
@@ -73,31 +110,45 @@ const RenderCatalog = (self, fold) => ({
         tabArea.classList.add('Tabel');
         container.appendChild(tabArea);
 
-        window.addEventListener('load', () => ((parent) => {
-            parent.style.display = 'flex';
-            parent.insertBefore(container, Markplus.container);
-        })(Markplus.container.parentElement));
+        const scrollTo = target => {
+            const styleTop = () => Math.min(target.offsetTop - target.offsetParent.offsetTop, container.clientHeight - tabArea.clientHeight);
+            target && target.offsetParent && [tabArea, foldButton].forEach(dom => dom.style.top = \`\${styleTop()}px\`);
+        }
 
+        const backTop = document.createElement('span');
+        container.appendChild((button => {
+            button.innerText = '︽';
+            button.classList.add('top');
+            button.addEventListener('click', () => {
+                location.hash = location.hash ? '' : '#top';
+                scrollTo(Markplus.container);
+            });
+            ${fold ? 'container.classList.add(\'fold\');' : ''}
+            return button;
+        })(backTop));
+
+        const hashAvailable = () => location.hash && location.hash != '#top';
         const dynamicStyle = document.querySelector('#markplusCatalogDynamicStyle');
         ['load', 'hashchange'].forEach(event => window.addEventListener(event, () => {
             const firstTab = container.querySelector('.Tabel span.tab');
-            const hash = decodeURIComponent(location.hash || firstTab && firstTab.getAttribute('to'));
+            const hash = decodeURIComponent(hashAvailable() ? location.hash : firstTab && firstTab.getAttribute('to'));
+            console.log(hash);
             dynamicStyle.innerHTML = \`.Markplus-catalog>.Tabel span.tab[to="\${hash}"] { color: #fff; }\`;
-            const hashTag = Markplus.container.querySelector(\`.Header\${hash}\`);
-            const styleTop = () => Math.min(hashTag.offsetTop - hashTag.offsetParent.offsetTop, container.clientHeight - tabArea.clientHeight);
-            hashTag && (tabArea.style.top = foldButton.style.top = \`\${styleTop()}px\`);
+            hashAvailable() && scrollTo(Markplus.container.querySelector(\`.Header\${hash}\`) || Markplus.container.querySelector(hash) || document.querySelector(hash));
         }));
 
         const catalog = /** @param {HTMLElement} ele */ ele => {
             if (!ele) {
                 return container;
             }
+            ele.addEventListener('click', () => scrollTo(ele));
+
             const tab = document.createElement('span');
             tab.innerText = ele.innerText;
             tab.classList.add('tab');
             tab.classList.add(\`tab-\${ele.getAttribute('data-markplus-header-level')}\`);
             tab.setAttribute('to', \`#\${ele.id}\`);
-            tab.addEventListener('click', () => location.hash = ele.id)
+            tab.addEventListener('click', () => location.hash = ele.id);
             tabArea.appendChild(tab);
         };
         Markplus.decorators.push(ele => ele.classList.contains('Header') && catalog(ele));
